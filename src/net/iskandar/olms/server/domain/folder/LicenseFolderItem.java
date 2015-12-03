f60ac6152e0d5d32c90e0351d7fa3c00709980e2
iskandar <izaynutdinov@gmail.com>
2015-12-03 21:13:21 +0500

    Fixes

--- src/net/iskandar/olms/server/domain/folder/LicenseFolderItem.java
+++ src/net/iskandar/olms/server/domain/folder/LicenseFolderItem.java
@@ -4,7 +4,6 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import net.iskandar.olms.server.domain.License;
 
 @Entity
